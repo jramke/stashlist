47a29e6b31603cb2bf6f87210b3eabeb705dbf9d
jramke <jstrmk@gmail.com>
2024-03-24 15:45:53 +0100

    Fix: 404 error link

--- apps/web/src/routes/+error.svelte
+++ apps/web/src/routes/+error.svelte
@@ -1,13 +1,20 @@
 <script>
+	import { page } from '$app/stores';
+	import { siteConfig } from '$lib/config/site';
 	import Section from '@repo/ui/components/section';
 	import Heading from '@repo/ui/components/heading';
 	import { Button } from '@repo/ui/components/button';
+	
 </script>
 
 <Section>
 	<div class="container flex min-h-[70vh] max-w-[800px] flex-col items-center justify-center gap-6">
 		<p class="text-lg text-muted-foreground">404 Error</p>
 		<Heading tag="h1" class="mb-3">Page not found!</Heading>
-		<Button href={'/'}>Go back</Button>
+		{#if $page.data.user}
+			<Button href={siteConfig.appUrl}>Go back</Button>
+		{:else}
+			<Button href={'/'}>Go back</Button>
+		{/if}
 	</div>
 </Section>
