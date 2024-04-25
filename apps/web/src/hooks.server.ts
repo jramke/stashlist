a933b248cc29d9fff239673de672c366df5afa71
jramke <jstrmk@gmail.com>
2024-04-25 19:20:53 +0200

    Fix csrf

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -4,10 +4,46 @@ import { redirect, error, json, text, type Handle } from '@sveltejs/kit';
 import { CHROME_EXTENSION_ID, CHROME_EXTENSION_ID_DEV } from "$env/static/private";
 
 export const handle: Handle = async ({ event, resolve }) => {
+	const allowedPaths = [
+		'/api/saves/new/website',
+		'/api/saves/new/image',
+	];
+
+	// https://github.com/sveltejs/kit/issues/6784
+	const forbidden =
+		event.request.method === 'POST' &&
+		event.request.headers.get('origin') !== event.url.origin &&
+		isFormContentType(event.request) &&
+		!allowedPaths.includes(event.url.pathname);
+
+	if (forbidden) {
+		const csrfError = error(
+			403,
+			`Cross-site ${event.request.method} form submissions are forbidden`,
+		);
+		if (event.request.headers.get('accept') === 'application/json') {
+			return json(csrfError.body, { status: csrfError.status });
+		}
+		return text(csrfError.body.message, { status: csrfError.status });
+	}
+
+	const requestPath = event.url.pathname;
+	const isRequestToApi = requestPath.startsWith('/api');
+	const declineApiRequests = () => {
+		if (!isRequestToApi) return;
+		if (allowedPaths.includes(requestPath)) {
+			error(401, 'You are not authorized. Please log in.');
+		} else {
+			error(404, 'Not found');
+		}
+	};
+
 	const sessionId = event.cookies.get(lucia.sessionCookieName);
+
 	if (!sessionId) {
 		event.locals.user = null;
 		event.locals.session = null;
+		declineApiRequests();
 		return await resolve(event);
 	}
 
@@ -34,6 +70,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 			path: '.',
 			...sessionCookie.attributes
 		});
+		declineApiRequests();
 		if (event.url.pathname.startsWith(siteConfig.appUrl)) {
 			redirect(302, '/login');
 		}
@@ -42,59 +79,16 @@ export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.user = user;
 	event.locals.session = session;
 
-	// return await resolve(event);
-
-	// handle cors
-	// // TODO: tighten before prod
-	// // https://snippets.khromov.se/configure-cors-in-sveltekit-to-access-your-api-routes-from-a-different-host/
-	// // TODO: check if this check is better https://github.com/sveltejs/kit/issues/6784#issuecomment-1416104897
-	// if (event.url.pathname.startsWith('/api')) {
-	// 	// Required for CORS to work
-	// 	if (event.request.method === 'OPTIONS') {
-	// 		return new Response(null, {
-	// 			headers: {
-	// 				'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
-	// 				'Access-Control-Allow-Origin': '*',
-	// 				'Access-Control-Allow-Headers': '*'
-	// 			}
-	// 		});
-	// 	}
-	// }
-
-	// const response = await resolve(event);
-	// if (event.url.pathname.startsWith('/api')) {
-	// 	response.headers.append('Access-Control-Allow-Origin', `*`);
-	// }
-	const chromeId = import.meta.env.DEV ? CHROME_EXTENSION_ID_DEV : CHROME_EXTENSION_ID;
-	const allowedOrigins = [
-		`chrome-extension://${chromeId}`,
-		// 'moz-extension://YOUR_FIREFOX_EXTENSION_ID'
-	];
-	const allowedPaths = [
-		'/api/saves/new/website',
-		'/api/saves/new/image',
-	];
-	const origin = event.request.headers.get('origin') || '';
-	console.log('-------');
-	console.log('origin', origin);
-	console.log('allowedOrigins', allowedOrigins);
-	console.log('event.url.origin', event.url.origin);
-	console.log('event.url.pathname', event.url.pathname);
-	console.log('-------');
-	const forbidden = event.request.method === 'POST' && origin !== event.url.origin && !(allowedOrigins.includes(origin) && allowedPaths.includes(event.url.pathname));
-
-	if (forbidden) {
-		const csrfError = error(
-			403,
-			`Cross-site ${event.request.method} requests are forbidden from ${origin} to ${event.url.origin}`,
-		);
-		if (event.request.headers.get('accept') === 'application/json') {
-			return json(csrfError.body, { status: csrfError.status });
-		}
-		return text(csrfError.body.message, { status: csrfError.status });
-	}
-
 	const response = await resolve(event);
 
 	return response;
 };
+
+
+function isContentType(request: Request, ...types: string[]) {
+	const type = request.headers.get('content-type')?.split(';', 1)[0].trim() ?? '';
+	return types.includes(type);
+}
+function isFormContentType(request: Request) {
+	return isContentType(request, 'application/x-www-form-urlencoded', 'multipart/form-data');
+}
