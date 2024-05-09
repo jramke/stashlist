0b03fafbdaab5fb4eefbcbd8b89f7c884999eb71
jramke <jstrmk@gmail.com>
2024-05-09 19:23:11 +0200

    Update footer

--- apps/web/src/lib/components/site/footer.svelte
+++ apps/web/src/lib/components/site/footer.svelte
@@ -12,16 +12,31 @@
 
 <Section class="border-t">
     <footer class="container">
-        <div class="w-full flex flex-col flex-wrap gap-4">
-            <a href={$page.data.user ? siteConfig.appUrl : '/'} class="flex items-center gap-2">
-                <Logo class="h-5 w-5" />
-                <span class="text-lg font-bold sm:inline-block">
-                    {siteConfig.name}
+        <div class="grid md:grid-cols-2 gap-6">
+            <div class="flex flex-col flex-wrap justify-end gap-2">
+                <a href={$page.data.user ? siteConfig.appUrl : '/'} class="flex items-center gap-2">
+                    <Logo class="h-5 w-5" />
+                    <span class="text-lg font-bold sm:inline-block">
+                        {siteConfig.name}
+                    </span>
+                </a>
+                <span class="text-sm text-muted-foreground">
+                    Made by 
+                    <a href="https://joostramke.com" target="_blank" class="underline underline-offset-4">
+                        Joost
+                    </a>
                 </span>
-            </a>
-            <span class="text-sm text-muted-foreground">
-                Copyright © {new Date().getFullYear()} Stashlist. All rights reserved.
-            </span>
+            </div>
+            <div class="flex flex-col flex-wrap justify-end gap-2 md:text-right">
+                <div class="text-sm text-muted-foreground">
+                    <a href="/privacy" class="underline underline-offset-4">
+                        Privacy Policy
+                    </a>
+                </div>
+                <span class="text-sm text-muted-foreground">
+                    Copyright © {new Date().getFullYear()} Stashlist. All rights reserved.
+                </span>
+            </div>
         </div>
     </footer>
 </Section>
