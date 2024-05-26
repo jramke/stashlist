c5f069a75033fe968ac98837d711429ae70715c1
jramke <jstrmk@gmail.com>
2024-05-26 21:48:01 +0200

    Updated landing pag, closes #37

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -3,11 +3,12 @@
 	import { Logo, GitHub } from '@repo/ui/icons';
 	import { TwitterBookmark, Chrome, Codepen, Dribbble, Behance, GithubStar, Arc, LogoPoof } from '$lib/components/site/logos';
 	import Typography from '@repo/ui/components/typography';
+	import { Button } from '@repo/ui/components/button';
 
 
 </script>
 
-<Section class="py-20">
+<Section class="pt-20">
 	<Typography class="container">
 		<div class="text-2xl leading-normal font-medium space-y-10">
 			<h1 class="font-bold text-4xl">
@@ -25,14 +26,14 @@
 					</LogoPoof>
 					browser 
 				</span>
-				bookmarks scattered like confetti, 
+				and  
 				<span class="whitespace-nowrap"> 
 					<LogoPoof>
 						<TwitterBookmark class="h-7 w-7 inline -mt-2" /> 
 					</LogoPoof>
 					Twitter
 				</span>
-				bookmarks lost in the noise, and a graveyard of your liked 
+				bookmarks scattered like confetti, and a graveyard of your liked 
 				<span class="whitespace-nowrap"> 
 					<LogoPoof>
 						<Codepen class="h-7 w-7 inline -mt-2 text-foreground" /> 
@@ -62,11 +63,11 @@
 					</LogoPoof>
 					GitHub
 				</span>  
-				stars, searching for that damn snippet or library.
+				stars, searching for that damn library.
 			</p>
 			<p>
-				Sound familiar? It's the repeating cycle of searching, clicking, and hoping you'll stumble upon what you need. 
-				<br>
+				<!-- Sound familiar? It's the repeating cycle of searching, clicking, and hoping you'll stumble upon what you need. 
+				<br> -->
 				But what if there was a better way?
 			</p>
 			<h2 class="font-bold text-2xl">
@@ -74,4 +75,51 @@
 			</h2>
 		</div>
 	</Typography>
+</Section>
+<Section>
+	<div class="container">
+		<img class="border rounded-xl" src="/screenshot.png" alt="Screenshot of the stashlist app UI">
+	</div>
+</Section>
+<Section>
+	<Typography class="container">
+		<div class="text-xl leading-normal font-medium space-y-10">
+			<h2 class="font-bold text-3xl mt-0">
+				Features
+			</h2>
+			<p>
+				Minimalistic UI without missing any feature to help you organize your bookmarks.
+			</p>
+			<p>
+				Keyboard accessibility and command menu with a lot of shortcuts for a faster workflow and navigation.
+			</p>
+			<p>
+				Grouping system to to help you categorize your bookmarks.
+			</p>
+			<p>
+				Browser extension to save wesbites or images directly from your browser.
+			</p>
+			<p>
+				Automatically fetches metadata for your bookmarks.
+			</p>
+			<p>
+				Get more information about the features and functionality <a class="underline underline-offset-4" href="/help">here</a>.
+			</p>
+		</div>
+	</Typography>
+</Section>
+<Section class="py-16">
+	<div class="container">
+		<h2 class="font-bold text-3xl text-center">
+			Go ahead, give it a try!
+		</h2>
+		<div class="flex justify-center gap-3 items-center mt-8">
+			<Button href="/login">
+				Getting started
+			</Button>
+			<Button href="https://github.com/jramke/stashlist" variant="link" target="_blank">
+				View on Github
+			</Button>
+		</div>
+	</div>
 </Section>
