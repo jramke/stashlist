1c14cb0c7265d5cc0fdc940fe5123b94621beedf
jramke <jstrmk@gmail.com>
2024-07-05 17:21:46 +0200

    [Web] Update user info

--- apps/web/src/lib/components/app/nav/userinfo.svelte
+++ apps/web/src/lib/components/app/nav/userinfo.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { buttonVariants } from '@repo/ui/components/button';
-    import { LogOut, GitHub, Google, Info } from '@repo/ui/icons';
+    import { LogOut, GitHub, Google, Info, Command, Blocks, Unplug } from '@repo/ui/icons';
     import { enhance } from '$app/forms';
 	import * as DropdownMenu from '@repo/ui/components/dropdown-menu';
     import { page } from '$app/stores';
@@ -45,30 +45,43 @@
             <Avatar.Fallback>{user.name.slice(0, 2)}</Avatar.Fallback>
         </Avatar.Root>
     </DropdownMenu.Trigger>
-    <DropdownMenu.Content align="end" class="min-w-[14rem]">
+    <DropdownMenu.Content align="end" class="min-w-[15rem]">
         <DropdownMenu.Label>
-            <span class="text-lg">{user.name}</span>
+            <span class="text-base">{user.name}</span>
             <div class="text-muted-foreground flex items-center gap-1">
                 {#if userProvider === 'google'}
-                    <Google class="size-4" />
+                    <Google class="size-3" />
                 {:else if userProvider === 'github'}
-                    <GitHub class="size-4" />
+                    <GitHub class="size-3" />
                 {/if}
-                <span>{user.username}</span>
+                <span class="text-xs">{user.username}</span>
             </div>
         </DropdownMenu.Label>
         <DropdownMenu.Separator />
         <DropdownMenu.Group>
-            <DropdownMenu.Item on:click={() => generateKeyDialogOpen.set(true)}>Generate API key</DropdownMenu.Item> 
-            <DropdownMenu.Item href={'/extension'}>Get the extension</DropdownMenu.Item> 
+            <DropdownMenu.Item on:click={() => generateKeyDialogOpen.set(true)}>
+                <Unplug class="me-2 h-4 w-4" />
+                Generate API key
+            </DropdownMenu.Item> 
+            <DropdownMenu.Sub>
+                <DropdownMenu.SubTrigger>
+                    <Blocks class="mr-2 h-4 w-4" />
+                    <span>Products</span>
+                </DropdownMenu.SubTrigger>
+                <DropdownMenu.SubContent side="left">
+                    <DropdownMenu.Item href={'/extension'}>Browser extension</DropdownMenu.Item> 
+                    <DropdownMenu.Item href={'https://github.com/jramke/stashlist/releases/latest'}>Desktop launcher</DropdownMenu.Item> 
+                </DropdownMenu.SubContent>
+            </DropdownMenu.Sub>
             <DropdownMenu.Item on:click={() => commandMenuOpen.set(true)}>
+                <Command class="me-2 h-4 w-4" />
                 Command Menu
                 <DropdownMenu.Shortcut>
                     <Shortcut keys={['command', 'K']} />
                 </DropdownMenu.Shortcut>
             </DropdownMenu.Item> 
             <DropdownMenu.Item href={'/help'}>
-                <!-- <Info class="me-2 h-4 w-4" /> -->
+                <Info class="me-2 h-4 w-4" />
                 Help
                 <DropdownMenu.Shortcut>
                     <Shortcut keys={['command', 'H']} />
