fdbc3736214cfcdf463f4af60c7ae99972ea9607
jramke <jstrmk@gmail.com>
2024-07-05 18:51:18 +0200

    Move edit dialog to layout, fixes #69

--- apps/web/src/lib/components/app/saves/list.svelte
+++ apps/web/src/lib/components/app/saves/list.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { invalidateAll } from '$app/navigation';
 	import { siteConfig } from '@repo/constants';
 	import { listLayout, listColumns, itemsStore } from '$lib/stores';
 	import { EmptyState } from '$lib/components/app';
     import { Item } from '$lib/components/app/saves';
 	import Masonry from '@repo/ui/components/masonry';
-	import { Button, buttonVariants } from '@repo/ui/components/button';
-	import * as Drawer from '@repo/ui/components/drawer';
-	import EditForm from '$routes/main/save/edit/[id]/+page.svelte';
+	import { Button } from '@repo/ui/components/button';
 	import { gridArrowKeys } from '$lib/actions';
-	import { toast } from '@repo/ui/components/sonner';
-	import { ScrollArea } from '@repo/ui/components/scroll-area';
 
     type SaveListOptions = 'all' | 'savesByGroup' | 'unsorted';
     export let saves: SaveListOptions = 'all';
@@ -37,21 +32,6 @@
 		const items = $page.data.saves?.items || [];
 		if (!items || items.length === 0) return [];
 		return items.filter(save => save.saveGroups.length === 0);
-	}
-
-	async function editDialogChange(open: boolean) {
-		if (!open) {
-			if ($page.state.editStash) {
-				await invalidateAll(); // if this is not there error: "pushstate Could not serialize state.form"
-				history.back();
-			}
-			if ($focusedItem) {
-				$focusedItem?.focus();
-				focusedItem.set(null);
-			}
-			$editDialogCloseCallback?.();
-			editDialogCloseCallback.set(null);
-		}
 	}
 
 </script>
@@ -86,27 +66,4 @@
 			<Button href="/extension" variant="outline">Get the extension</Button>
 		</EmptyState>
 	{/if}
-{/if}
-
-{#if $page.state.editStash}
-	<Drawer.Root bind:open={$editDialogOpen} onOpenChange={editDialogChange} direction="right">
-		<Drawer.Content layoutDirection={'right'}>
-			<ScrollArea class="h-full">
-				<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-				<div tabindex="0" class="outline-none flex flex-col flex-grow h-full">
-					<Drawer.Header>
-						<Drawer.Title>Detail view</Drawer.Title>
-					</Drawer.Header>
-					<div class="p-4 flex flex-col flex-grow">
-						<EditForm data={$page.state.editStash}>
-							<Drawer.Footer class="p-0 pt-4 grid grid-cols-2">
-								<Drawer.Close class={buttonVariants({variant: 'outline'})}>Cancel</Drawer.Close>
-								<Button type="submit" form="edit-stash-form">Update</Button>
-							</Drawer.Footer>
-						</EditForm>
-					</div>
-				</div>
-			</ScrollArea>
-		</Drawer.Content>
-	</Drawer.Root>
 {/if}
--- apps/web/src/routes/main/+layout.svelte
+++ apps/web/src/routes/main/+layout.svelte
@@ -2,7 +2,30 @@
 	import Section from '@repo/ui/components/section';
 	import { Topbar } from '$lib/components/app/nav';
 	import { CommandMenu } from '$lib/components/app/command';
+	import { page } from '$app/stores';
+	import { itemsStore } from '$lib/stores';
 	import { ScrollArea } from '@repo/ui/components/scroll-area';
+	import { Button, buttonVariants } from '@repo/ui/components/button';
+	import * as Drawer from '@repo/ui/components/drawer';
+	import EditForm from '$routes/main/save/edit/[id]/+page.svelte';
+	import { invalidateAll } from '$app/navigation';
+
+	const { editDialogOpen, focusedItem, editDialogCloseCallback } = itemsStore;
+
+	async function editDialogChange(open: boolean) {
+		if (!open) {
+			if ($page.state.editStash) {
+				await invalidateAll(); // if this is not there error: "pushstate Could not serialize state.form"
+				history.back();
+			}
+			if ($focusedItem) {
+				$focusedItem?.focus();
+				focusedItem.set(null);
+			}
+			$editDialogCloseCallback?.();
+			editDialogCloseCallback.set(null);
+		}
+	}
 
 </script>
 
@@ -18,3 +41,26 @@
 		<!-- </ScrollArea> -->
 	</Section>
 </Section>
+
+{#if $page.state.editStash}
+	<Drawer.Root bind:open={$editDialogOpen} onOpenChange={editDialogChange} direction="right">
+		<Drawer.Content layoutDirection={'right'}>
+			<ScrollArea class="h-full">
+				<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+				<div tabindex="0" class="outline-none flex flex-col flex-grow h-full">
+					<Drawer.Header>
+						<Drawer.Title>Detail view</Drawer.Title>
+					</Drawer.Header>
+					<div class="p-4 flex flex-col flex-grow">
+						<EditForm data={$page.state.editStash}>
+							<Drawer.Footer class="p-0 pt-4 grid grid-cols-2">
+								<Drawer.Close class={buttonVariants({variant: 'outline'})}>Cancel</Drawer.Close>
+								<Button type="submit" form="edit-stash-form">Update</Button>
+							</Drawer.Footer>
+						</EditForm>
+					</div>
+				</div>
+			</ScrollArea>
+		</Drawer.Content>
+	</Drawer.Root>
+{/if}
