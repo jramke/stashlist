fd7c1397e226f429c94c66476eb2ee835bffb9d5
jramke <jstrmk@gmail.com>
2024-07-21 16:02:54 +0200

    Use turso embedded replica

--- apps/web/src/lib/server/db/index.ts
+++ apps/web/src/lib/server/db/index.ts
@@ -3,6 +3,12 @@ import { createClient } from '@libsql/client';
 import { env } from '$env/dynamic/private';
 import * as schema from './schema';
 
-const client = createClient({ url: env.DATABASE_URL, authToken: env.DATABASE_AUTH_TOKEN });
+const client = createClient({ 
+    url: "file:stashlist-replica.db",
+    syncUrl: env.DATABASE_URL, 
+    authToken: env.DATABASE_AUTH_TOKEN,
+    encryptionKey: env.DATABASE_REPLICA_ENCRYPTION_KEY,
+    syncInterval: 60,
+});
 
 export const db = drizzle(client, { schema });
