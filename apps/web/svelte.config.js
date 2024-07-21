0c552e43159e1e061854c08a478838bbb3e7acbb
jramke <jstrmk@gmail.com>
2024-07-21 09:18:00 +0200

    Use node adapter for web

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,26 +1,13 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-const { DEV } = process.env;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: [vitePreprocess({})],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			regions: ['fra1']
-		}),
+		adapter: adapter(),
 		csrf: {
-			// handled manually in hooks // https://github.com/sveltejs/kit/issues/6784
-			// checkOrigin: +DEV ? false : true 
 			checkOrigin: false,
 		},
 		alias: {
